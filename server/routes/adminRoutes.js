fef6bc1bed28d22f02067a63af3d1f231ace5920
abdhullah200 <abdhullahariff118@gmail.com>
2025-09-24 00:51:17 +0530

    Create API for blog comments & Connect Backend APIs with Frontend blog app

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import { adminLogin } from '../controllers/adminController.js';
+import { 
+    adminLogin, 
+    getAllBlogsAdmin, 
+    getBlogByIdAdmin, 
+    deleteBlogByIdAdmin, 
+    approveCommentAdmin, 
+    getDashboardDataAdmin 
+} from '../controllers/adminController.js';
+import auth from '../middleware/auth.js';
 
 const adminRouter=express.Router();
 
 adminRouter.post("/login",adminLogin);
+adminRouter.get("/blogs",auth,getAllBlogsAdmin);
+adminRouter.get("/blogs/:blogId",auth,getBlogByIdAdmin);
+adminRouter.post("/delete",auth,deleteBlogByIdAdmin);
+adminRouter.post("/approve-comment",auth,approveCommentAdmin);
+adminRouter.get("/dashboard",auth,getDashboardDataAdmin);
+
 
 export default adminRouter;
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -5,11 +5,13 @@ import auth from "../middleware/auth.js";
 
 const blogRouter= express.Router();
 
+
 blogRouter.post("/add",upload.single("image"),auth, addBlog);
 blogRouter.get("/all", getAllBlogs);
 blogRouter.get("/:blogId", getBlogById);
 blogRouter.post("/delete",auth, deleteBlogById);
 blogRouter.post("/toggle-public",auth, togglePublic);
+
 
 export default blogRouter;
 
